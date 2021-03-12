4d1b90d351e8a0da288b5d1b475e2c3170a0404e
centroid457 <centroid@mail.ru>
2021-03-12 17:06:39 +0300

    correct! socket!

--- test-util.py
+++ test-util.py
@@ -2,10 +2,8 @@
 
 import sys
 import socket
-import struct
 import re
 import contracts
-import subprocess
 import pathlib
 import requests
 
@@ -29,24 +27,8 @@ if len(sys.argv) == 3:
 path_to_save_results.mkdir(exist_ok=True)
 
 
-# [+WIN/?Lin]------------------------------------------------------
-# 2=GET IP = DONT NEED
-"""
-@contracts.contract(mac=str, returns="None|str")
-def dev_get_ip_from_mac(mac):
-    arp_sp = subprocess.Popen(f"arp -a", text=True, shell=True, stdout=subprocess.PIPE)
-    arp_readlines = arp_sp.stdout.readlines()
-
-    for line in arp_readlines:
-        if mac in line:
-            ip = line.split()[0]
-            print(ip)
-            return ip
-    return None
-"""
-
 # [+?]-----------------------------------------------------
-# 3=DEV TEST=json_rpc
+# 2=DEV TEST=json_rpc
 @contracts.contract(ip=str, dev_id=int, returns="int|None")
 def dev_test_start(ip, dev_id):
     dev_url = f"http://{ip}/api"
@@ -59,7 +41,7 @@ def dev_test_start(ip, dev_id):
     try:
         # response_http_json = response_http.json()                             #del by corrector
         # response_json_rpc = response_http_json.get("data", {"result": -1})    #del by corrector
-        response_json_rpc = response_http.json()        #add by corrector
+        response_json_rpc = response_http.json()        # add by corrector
 
         if response_json_rpc["jsonrpc"] == "2.0" and response_json_rpc["id"] == dev_id:
             response_dev_result = response_json_rpc["result"]
@@ -69,7 +51,7 @@ def dev_test_start(ip, dev_id):
 
 
 # [+]------------------------------------------------------
-# 4=SAVE RESULT
+# 3=SAVE RESULT
 @contracts.contract(mac=str)
 def dev_test_save_result(mac):
     dev_dict = dev_found_mac_dict[mac]
@@ -91,19 +73,16 @@ def dev_test_save_result(mac):
 
 
 # [+]------------------------------------------------------
-# 5=UDP LISTEN
+# 4=UDP LISTEN
 def udp_listen():
     json_rpc_dev_id = 0
 
-    udp_multicast = socket.socket(socket.AF_INET, socket.SOCK_DGRAM, socket.IPPROTO_UDP)
+    udp_multicast = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
     udp_multicast.setsockopt(socket.SOL_SOCKET, socket.SO_REUSEADDR, 1)
     udp_multicast.bind(('', 12345))
-    mreq = struct.pack("=4sl", socket.inet_aton("225.1.1.1"), socket.INADDR_ANY)
-    udp_multicast.setsockopt(socket.IPPROTO_IP, socket.IP_ADD_MEMBERSHIP, mreq)
 
     while True:
-        # udp_get_msg_b = udp_multicast.recv(10240)     #del by corrector
-        udp_get_msg_b, (dev_ip, _) = udp_multicast.recvfrom(10240)  #add by corrector
+        udp_get_msg_b, (dev_ip, _) = udp_multicast.recvfrom(1024)
         udp_get_msg = udp_get_msg_b.decode("utf-8")
         # print(udp_get_msg)
 
@@ -117,10 +96,9 @@ def udp_listen():
             dev_sn = match[3]
 
             if dev_model in models_used_list:
-                print("found: ", dev_mac, dev_sn)
+                print("Get UDP: ", dev_ip, dev_mac, dev_sn)
 
                 if dev_mac not in dev_found_mac_dict:
-                    #dev_ip = dev_get_ip_from_mac(dev_mac)  #del by corrector
                     if dev_ip is None:
                         continue
 
@@ -129,3 +107,6 @@ def udp_listen():
                     dev_found_mac_dict[dev_mac].update({"result": dev_test_result})
 
                     dev_test_save_result(dev_mac)
+
+
+udp_listen()
